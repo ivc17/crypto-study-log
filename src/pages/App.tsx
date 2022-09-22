26b260599fa6784a09d69fcc61f2a77a4819c4c8
ivc17 <ivc1741@gmail.com>
2022-09-22 16:42:38 +0800

    fix path

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,7 +4,7 @@ import Home from './Home'
 function App() {
   return (
     <Routes>
-      <Route path={'/crypto-study-log/*'} element={<Home />} />
+      <Route path={'/*'} element={<Home />} />
       {/* <Route path="*" element={<Navigate to={'/'} replace />} /> */}
     </Routes>
   )
