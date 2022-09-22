1242e3efedba75e94faa2d841fc82ea462303c6d
ivc17 <ivc1741@gmail.com>
2022-09-22 16:07:53 +0800

    fix base url

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/crypto-study-log">
         <App />
       </BrowserRouter>
     </ThemeProvider>
